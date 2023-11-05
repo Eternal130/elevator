2b7f162e7f7ec71dbea61dc40769e8e8b2ccc548
Eternal130 <111716703+Eternal130@users.noreply.github.com>
2023-11-05 16:14:40 +0800

    完善事件集(部分)

--- 事件集.py
+++ 事件集.py
@@ -22,3 +22,41 @@ def EAFF(状态集: 状态集, f: int):  # 有电梯到达f层
     for d in [0, 1]:
         if 状态集.FB[f][d] and any([S(状态集, e, f, d) for e in range(len(状态集.S))]):
             状态集.FB[f][d] = False
+
+
+def DC(状态集: 状态集, e: int, f: int):  # 电梯e在楼层f关门
+    if 状态集.S[e][f][0]:
+        状态集.M[e][f - 1][0] = True
+    elif 状态集.S[e][f][1]:
+        状态集.M[e][f + 1][1] = True
+    elif 状态集.S[e][f][-1]:
+        状态集.W[e][f] = True
+
+
+def ST(状态集: 状态集, e: int, f: int):  # 电梯e靠近f层时触发传感器，电梯控制器决定是否在当前楼层停下
+    for d in [0, 1]:
+        if 状态集.FB[d][f] or 状态集.EB[e][f]:  # 电梯e要在f层停下
+            # 状态集.S[e][f][d] = True
+            状态集.M[e][f][d] = False
+            # 状态集.W[e][f] = False
+            状态集.FB[d][f] = False
+            状态集.EB[e][f] = False
+            DC(状态集, e, f)
+            return
+        else:  # 电梯e不在f层停下
+            状态集.M[e][f][d] = False
+            状态集.M[e][f + 1 if d > 0 else f - 1][d] = True
+            状态集.S[e][f][d] = False
+            状态集.W[e][f] = False
+
+
+def RL(状态集: 状态集, e: int, f: int, d: int, E: bool):#电梯或楼层按钮被按下，E为True表示按下的是电梯内按钮，否则按下的是楼层按钮
+    if E:
+        EBP(状态集, e, f)
+        #如果电梯处于等待状态，且电梯内按钮被按下，则电梯开始运行
+        if 状态集.W[e][f]:
+            状态集.W[e][f] = False
+            状态集.S[e][f][d] = True
+            状态集.M[e][f][d] = True
+    else:
+        FBP(状态集, d, f)
--- 谓词集.py
+++ 谓词集.py
@@ -7,6 +7,6 @@ def V(状态集: 状态集, e: int, f: int):#电梯e是否停在f层
 
 def S(状态集: 状态集, e: int, f: int, d: int):#电梯e停在f层且移动方向由d确认，0向下，1向上，-1待定
     if d == -1:
-        return 状态集.S[e][f][0] or 状态集.S[e][f][1] or 状态集.W[e][f]
+        return not (状态集.S[e][f][0] or 状态集.S[e][f][1])
     return 状态集.S[e][f][d]
 
